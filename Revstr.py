c34994d81a485dcf52da0e8b62f2022ded5c3737
saurabhB0970 <bhosalesaurabh645@gmail.com>
2025-06-05 14:18:14 +0530

    Add files via upload

---
+++ Revstr.py
@@ -0,0 +1,14 @@
+s="INSTAGRAM"
+print(s[-7:-4]); #prints STA
+print(s[-3:]); #print RAM
+print(s[-3:len(s)]); #print RAM
+print(s[-9:len(s)]); #print INSTAGRAM
+print(s[-9:]); #print INSTAGRAM
+print(s[-9: :-2]); #print I
+print(s[-9: :2]); #print ISARM
+print(s[-9:9:3]); #print ITR
+print(s[-7: :1]); #print STAGRAM
+print(s[-7:0:-1]); #prints SN
+print(s[-9:-1:-1]);#empty string
+print(s[-9:-1:1]); #print INSTAGRAM
+print(s[-1::-1]); #print MARGATSNI
