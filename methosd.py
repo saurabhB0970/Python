fafcb0bbc0b495fd7306ed58f9781c499443b80c
saurabhB0970 <bhosalesaurabh645@gmail.com>
2025-06-06 12:23:05 +0530

    Add files via upload

---
+++ methosd.py
@@ -0,0 +1,11 @@
+s="INs4TA5RAm"
+print(s)
+print(s.capitalize())#Ins4ta5ram
+print(s.islower())#False
+print(s.isalnum()) #True
+print(s.isalpha()) #False
+print(s.isupper()) #False
+print(s.isnumeric()) #false
+print(s.lower()) #ins4ta5ram
+print(s.upper()) #INS4TA5RAM
+
